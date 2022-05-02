40eb3dea9255457f6f4458fb9707fdf6f2514aa0
lolikonloli <1834269023@qq.com>
2022-05-02 21:24:47 +0800

    针对打包修改

--- main/main.py
+++ main/main.py
@@ -11,9 +11,9 @@ from create_suduku import create_sudu
 import qdarkstyle
 
 
-dirname = os.path.dirname(PySide2.__file__) 
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+# dirname = os.path.dirname(PySide2.__file__) 
+# plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 
 
